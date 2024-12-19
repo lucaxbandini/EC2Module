3bc0380b2168a0e103a29b6147422537f965c289
Luca Bandini <lucaxbandini@gmail.com>
2024-12-19 16:53:16 -0500

    changes

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
+resource "EC2" "this" {
 
-ami                         = "ami-02c21308fed24a8ab"
-instance_type               = "t2.micro"
-key_name                    = "LB-EC2-KP"
+}
+ami                         = var.ami
+instance_type               = t2.micro
+key_name                    = LB-EC2-KP
 monitoring                  = true
 vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
 subnet_id                   = aws_subnet.pri.id
 associate_public_ip_address = false
 
 tags = {
-  Name = "var.tags"
+  Name = var.tags
 }
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "ami" {
+  type        = string
+  description = "The AMI resource allows the creation and management of a completely-custom Amazon Machine Image (AMI). e.g. ami-02c21308fed24a8ab"
+  default     = ""
+}
+
+variable "monitoring" {
+  type    = bool
+  default = true
+}
