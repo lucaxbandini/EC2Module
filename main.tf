b452b7d7cc187afffeaffe097ab613e11e502dba
Luca Bandini <lucaxbandini@gmail.com>
2024-12-19 17:25:09 -0500

    made it generic

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "this" {
 
   ami                         = var.ami
   instance_type               = var.instance_type
-  key_name                    = var.key_name
+  key_name                    = var.kms_name
   monitoring                  = var.monitoring
   vpc_security_group_ids      = [aws_security_group.this.name]
   availability_zone           = var.availability_zone
