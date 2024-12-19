e98e4fefb16a657acdca04edead926d15380814f
Luca Bandini <lucaxbandini@gmail.com>
2024-12-19 16:54:50 -0500

    changes

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-resource "EC2" "this" {
+resource "aws_instance" "this" {
 
-}
-ami                         = var.ami
-instance_type               = t2.micro
-key_name                    = LB-EC2-KP
-monitoring                  = true
-vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-subnet_id                   = aws_subnet.pri.id
-associate_public_ip_address = false
+  ami                         = var.ami
+  instance_type               = t2.micro
+  key_name                    = LB-EC2-KP
+  monitoring                  = var
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  subnet_id                   = aws_subnet.pri.id
+  associate_public_ip_address = false
 
-tags = {
-  Name = var.tags
+  tags = {
+    Name = var.tags
+  }
 }
